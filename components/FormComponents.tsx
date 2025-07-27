2cf9b423d2c8beab04f77b3965ae8451cb49d4d5
rim4j <amirhosseinjalali818@gmail.com>
2025-07-27 11:06:29 +0330

    create custom form component

---
+++ components/FormComponents.tsx
@@ -0,0 +1,81 @@
+import { Control, FieldValues } from "react-hook-form";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "./ui/input";
+
+type CustomFormFieldProps = {
+  name: string;
+  control: Control<FieldValues>;
+};
+
+export const CustomFormField = ({ control, name }: CustomFormFieldProps) => {
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel className='capitalize'>{name}</FormLabel>
+          <FormControl>
+            <Input {...field} />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+};
+
+type CustomFormSelectProps = {
+  name: string;
+  control: Control<FieldValues>;
+  items: string[];
+  labelText?: string;
+};
+export const CustomFormSelect = ({
+  control,
+  items,
+  name,
+  labelText,
+}: CustomFormSelectProps) => {
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel className='capitalize'>{labelText || name}</FormLabel>
+          <Select onValueChange={field.onChange} defaultValue={field.value}>
+            <FormControl>
+              <SelectTrigger>
+                <SelectValue />
+              </SelectTrigger>
+            </FormControl>
+            <SelectContent>
+              {items.map((item) => {
+                return (
+                  <SelectItem key={item} value={item}>
+                    {item}
+                  </SelectItem>
+                );
+              })}
+            </SelectContent>
+          </Select>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+};
