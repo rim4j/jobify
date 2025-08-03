46d99f79c408b15707603855fdc00023e9cc16d6
rim4j <amirhosseinjalali818@gmail.com>
2025-08-03 08:27:21 +0330

    jobs list and job card

--- components/JobCard.tsx
+++ components/JobCard.tsx
@@ -1,7 +1,36 @@
 import { JobType } from "@/utils/types";
+import { MapPin, Briefcase, CalendarDays, RadioTower } from "lucide-react";
+
+import Link from "next/link";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Separator } from "./ui/separator";
+import { Button } from "./ui/button";
+import { Badge } from "./ui/badge";
+import JobInfo from "./JobInfo";
 
 const JobCard = ({ job }: { job: JobType }) => {
-  return <div>JobCard</div>;
+  return (
+    <Card className='bg-muted'>
+      <CardHeader>
+        <CardTitle>{job.position}</CardTitle>
+        <CardDescription>{job.company}</CardDescription>
+      </CardHeader>
+      <Separator />
+      <CardContent>{/* card info */}</CardContent>
+      <CardFooter className='flex gap-4'>
+        <Button asChild size='sm'>
+          <Link href={`/jobs/${job.id}`}>edit</Link>
+        </Button>
+      </CardFooter>
+    </Card>
+  );
 };
 
 export default JobCard;
--- components/JobsList.tsx
+++ components/JobsList.tsx
@@ -1,5 +1,47 @@
+"use client";
+import { getAllJobsAction } from "@/utils/actions";
+import { useQuery } from "@tanstack/react-query";
+import { useSearchParams } from "next/navigation";
+import JobCard from "./JobCard";
+
 const JobsList = () => {
-  return <div>JobsList</div>;
+  const searchParams = useSearchParams();
+  const search = searchParams.get("search") || "";
+  const jobStatus = searchParams.get("jobStatus") || "all";
+
+  const pageNumber = Number(searchParams.get("page")) || 1;
+
+  const { data, isPending } = useQuery({
+    queryKey: ["jobs", search, jobStatus, pageNumber],
+    queryFn: () =>
+      getAllJobsAction({
+        search,
+        jobStatus,
+        page: pageNumber,
+      }),
+  });
+
+  const jobs = data?.jobs || [];
+
+  if (isPending) {
+    return <h2 className='text-xl'>Loading...</h2>;
+  }
+
+  if (jobs.length < 1) {
+    return <h2 className='text-xl'>No jobs found ...</h2>;
+  }
+
+  return (
+    <>
+      {/* button container */}
+
+      <div className='grid md:grid-cols-2 gap-8'>
+        {jobs.map((item, i) => {
+          return <JobCard key={i} job={item} />;
+        })}
+      </div>
+    </>
+  );
 };
 
 export default JobsList;
